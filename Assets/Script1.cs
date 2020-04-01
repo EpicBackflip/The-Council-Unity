c91304ed8533448beb864568b2e11058fa493ed2
EpicBackflip <61590443+EpicBackflip@users.noreply.github.com>
2020-04-01 16:52:49 +0200

    alle box colliders

---
+++ Assets/Script1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script1 : MonoBehaviour
+{
+    public Transform Square;
+    float x;
+    float y;
+
+
+
+    void Start()
+    {
+
+
+
+
+    }
+
+
+
+
+
+    void Update()
+    {
+        /*
+        x = Input.mousePosition.x;
+        y = Input.mousePosition.y;
+       
+        if (x >= Square.position.x &&
+            x <= Square.position.x
+            && y >= Square.position.y &&
+            y <= Square.position.y)
+        {
+            Debug.Log(Square.position.x);
+        }
+
+       */
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log(Square.position.x);
+    }
+}
