8202898b4f7323a9827624e136482e52225a58ac
EpicBackflip <61590443+EpicBackflip@users.noreply.github.com>
2020-04-02 16:18:44 +0200

    main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+public void Playgame()
+    {
+
+
+        SceneManager.LoadScene(2);
+
+    }
+public void Tutorial()
+    {
+
+
+        SceneManager.LoadScene(1);
+    }
+
+public void Quit()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
