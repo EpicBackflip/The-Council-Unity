aaccc9fed049747fdf06fcdf5c27e2ab2ed0fb96
EpicBackflip <61590443+EpicBackflip@users.noreply.github.com>
2020-04-02 14:36:53 +0200

    bord erbij

---
+++ Assets/DDOL.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DDOL : MonoBehaviour
+{
+    public DDOL ddol;
+
+   public  float timer = 0;
+   public void Awake()
+    {
+        timer++;
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("DDOL"+gameObject.name);
+    
+
+    }
+}
---
+++ Assets/eventCard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eventCard : MonoBehaviour
+{
+    public float sec = 14f;
+ 
+    void Start()
+    {
+        if (gameObject.activeInHierarchy) {
+            gameObject.SetActive(false);
+        }
+
+
+        CancelInvoke();
+
+
+    }
+
+    void Update()
+    {
+        sec--;
+        if(sec <= 0)
+        {
+           // StartCoroutine(LateCall());
+            gameObject.SetActive(true);
+        }
+
+    }
+/*
+    IEnumerator LateCall()
+    {
+
+       
+
+        
+        //Do Function here...
+    }
+
+    */
+}
