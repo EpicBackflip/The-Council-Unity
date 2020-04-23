fd211127ca22bcab3f40f2e6117ad64ce9cf0cc8
EpicBackflip <61590443+EpicBackflip@users.noreply.github.com>
2020-04-23 14:10:44 +0200

    Movement werkt

--- Assets/CoinNieuwe.cs
+++ Assets/CoinNieuwe.cs
@@ -6,8 +6,8 @@ public class CoinNieuwe : MonoBehaviour
 {
   
     public  float movementSpeed = 5f;
-    public Vector3 Velocity = new Vector3(0, 5f, 0);
- 
+
+
 
     // Update is called once per frame
     public void Start()
@@ -17,18 +17,25 @@ public class CoinNieuwe : MonoBehaviour
     }
     public void Update()
     {
-        Debug.Log(transform.position);
+       
         float inputY = Input.GetAxis("Vertical");
-        //transform.position += new Vector3(0, inputY * movementSpeed * Time.deltaTime, 0);
-        transform.position = transform.position + new Vector3(0, inputY * movementSpeed * Time.deltaTime, 0);
+        Debug.Log(inputY);
+        transform.position = transform.position + new Vector3(0, inputY - movementSpeed * Time.deltaTime, 0);
+        //transform.position.y += inputY * movementSpeed * Time.deltaTime;
+
+        
+        
+
+
         if (transform.position.y < -10)
         {
             Reset();
         }
+        
     }
     public void Reset()
     {
-
+       
         transform.position = new Vector3(Random.Range(-13, 12), Random.Range(10, 15), transform.position.z);
     }
 }
