b0588ef756d0485564e21986f0281f70c7532ec1
EpicBackflip <61590443+EpicBackflip@users.noreply.github.com>
2020-04-23 13:50:18 +0200

    fucked coin movement

---
+++ Assets/Coin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public float gravityScale;
+    //public Rigidbody2D RB;
+    public Vector3 Velocity = new Vector3(0,0.0000000001f,0);
+    
+    //Random random = new Random();
+
+    void Update()
+    {
+        Debug.Log(transform.position);
+        //transform.position += Velocity;
+        //Debug.Log(transform.position);
+        if (transform.position.y < -10)
+        {
+            Reset();
+        }
+    }
+
+    void Reset()
+    {
+        //Debug.Log(RB.velocity);
+        transform.position = new Vector3(Random.Range(-13, 12), Random.Range(10, 15), transform.position.z);
+       
+        //RB.gravityScale = 0.1f;
+       //RB.velocity = new Vector2(0, 0.5f);
+    }
+}
---
+++ Assets/CoinNieuwe.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinNieuwe : MonoBehaviour
+{
+  
+    public  float movementSpeed = 5f;
+    public Vector3 Velocity = new Vector3(0, 5f, 0);
+ 
+
+    // Update is called once per frame
+    public void Start()
+    {
+        
+
+    }
+    public void Update()
+    {
+        Debug.Log(transform.position);
+        float inputY = Input.GetAxis("Vertical");
+        //transform.position += new Vector3(0, inputY * movementSpeed * Time.deltaTime, 0);
+        transform.position = transform.position + new Vector3(0, inputY * movementSpeed * Time.deltaTime, 0);
+        if (transform.position.y < -10)
+        {
+            Reset();
+        }
+    }
+    public void Reset()
+    {
+
+        transform.position = new Vector3(Random.Range(-13, 12), Random.Range(10, 15), transform.position.z);
+    }
+}
