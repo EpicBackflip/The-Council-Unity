11b251d6373e0b72b380d9a99ff1f22b0701d0c5
Jonah <jonah.h2012@gmail.com>
2020-04-30 18:19:01 +0200

    Zoom in prototype

---
+++ Assets/cameraZoomComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraZoomComponent : MonoBehaviour
+{
+    public Camera cam;
+    private float targetZoom;
+    private float zoomFactor = 80f;
+    [SerializeField] private float zoomLerpSpeed = 10f;
+
+    void Start()
+    {
+        targetZoom = cam.orthographicSize; 
+    }
+    void Update()
+    {
+        float scrollData = Input.GetAxis("Mouse ScrollWheel");    
+        targetZoom -= scrollData * zoomFactor;
+        targetZoom = Mathf.Clamp(targetZoom, 40f, 263f); 
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
+    }
+}
