6488bcd41ba8827e2457adbc3e0a3bce4fc43d63
EpicBackflip <61590443+EpicBackflip@users.noreply.github.com>
2020-05-29 00:33:10 +0200

    Geluiden toegevoegd

---
+++ Assets/GroeneBlokjesAudioDelayed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroeneBlokjesAudioDelayed : MonoBehaviour
+{
+    public  AudioSource GroeneAudio; 
+    void Start()
+    {
+
+       
+        GroeneAudio.PlayDelayed(2.0f);
+
+
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
