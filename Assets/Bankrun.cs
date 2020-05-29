eacbc9dae06a77d9bb2303323b0e1f7c91984f46
EpicBackflip <61590443+EpicBackflip@users.noreply.github.com>
2020-05-29 12:39:02 +0200

    Bank Audio

---
+++ Assets/Bankrun.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bankrun : MonoBehaviour
+{
+    
+    public AudioSource BankAudio;
+    public CameraZoomComponent cam;
+    void Start()
+    {
+
+
+        BankAudio.PlayDelayed(2.0f);
+
+
+    }
+
+
+    void Update()
+    {
+
+        if (cam.paused)
+        {
+
+            BankAudio.Pause();
+
+        }
+        else
+        {
+            BankAudio.UnPause();
+        }
+
+    }
+}
