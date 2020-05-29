a3b2218b39cd26069c70d98ef7a67110fe4eaa10
EpicBackflip <61590443+EpicBackflip@users.noreply.github.com>
2020-05-29 12:49:45 +0200

    Audio

---
+++ Assets/KaartenAudio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KaartenAudio : MonoBehaviour
+{
+    public AudioSource kaartenAudio;
+    public CameraZoomComponent cam;
+    void Start()
+    {
+
+
+        kaartenAudio.PlayDelayed(2.0f);
+
+
+    }
+
+
+    void Update()
+    {
+
+        if (cam.paused)
+        {
+
+            kaartenAudio.Pause();
+
+        }
+        else
+        {
+            kaartenAudio.UnPause();
+        }
+
+    }
+}
---
+++ Assets/KredietScore.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KredietScore : MonoBehaviour
+{
+    public AudioSource KredietAudio;
+    public CameraZoomComponent cam;
+    void Start()
+    {
+
+
+        KredietAudio.PlayDelayed(2.0f);
+
+
+    }
+
+
+    void Update()
+    {
+
+        if (cam.paused)
+        {
+
+            KredietAudio.Pause();
+
+        }
+        else
+        {
+            KredietAudio.UnPause();
+        }
+
+    }
+}
