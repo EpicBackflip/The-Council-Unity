ef5cc257ed3b5ede1aa3c198a037bc12deba7ed7
EpicBackflip <61590443+EpicBackflip@users.noreply.github.com>
2020-05-29 05:03:07 +0200

    WijkenUitleg toegevoegd

---
+++ Assets/WijkenUitleg.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WijkenUitleg : MonoBehaviour
+{
+    public AudioSource WijkenAudio;
+    void Start()
+    {
+        WijkenAudio.PlayDelayed(72f);
+    }
+
+
+    void Update()
+    {
+
+    }
+}
