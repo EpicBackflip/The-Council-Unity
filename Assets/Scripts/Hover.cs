6fbf2bb18a038274337f321a1f226c5251bfbefb
EpicBackflip <61590443+EpicBackflip@users.noreply.github.com>
2020-06-10 13:12:37 +0200

    update

--- Assets/Scripts/Hover.cs
+++ Assets/Scripts/Hover.cs
@@ -1,27 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Hover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    // The color of this panel will be used as a highlight
     public Image panel;
 
-    private float highlightColor = 0.05f; 
+    // The amount to highlight the panel
+    private float highlightColor = 0.05f;
 
-    Color tempColor = new Color(0,0,0,0); 
-    
-    // When the mouse hovers over the panel, highlight it. 
+    // When the mouse hovers over the panel, highlight it 
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
-        tempColor.a = highlightColor;
-        panel.color = tempColor;
+        panel.color = new Color(0, 0, 0, highlightColor);
     }
 
-    // And vice versa. 
+    // When the mouse hovers over the panel, don't highlight it 
     public void OnPointerExit(PointerEventData pointerEventData)
     {
         panel.color = new Color(0,0,0,0);
     }
 }
+
+//Rinske's code
--- Assets/Scripts/NavigationManager.cs
+++ Assets/Scripts/NavigationManager.cs
@@ -1,42 +1,50 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class NavigationManager : MonoBehaviour
 {
+    // Used to switch the current target
     public CameraZoomComponent zoom;
+    // Check if the tutorial is currently running 
     public ControlsTutorial controls;
+
+    // If the left side of the screen is pressed, go to the previous target
     public void Back()
     {
-        if(!checkIfCanProceed())
+        if (!CheckIfCanProceed())
+        {
             return;
+        }
 
-       zoom.PreviousTarget();
+        zoom.PreviousTarget();
     }
 
+    // If the center of the screen is pressed, the game will be paused or un-paused 
     public void Pause()
     {
-        if (!checkIfCanProceed())
+        if (!CheckIfCanProceed())
+        {
             return;
+        }
 
         zoom.Paused = !zoom.Paused;
     }
 
+    // If the right side of the screen is pressed, go to next target
     public void Next()
     {
-        if (!checkIfCanProceed())
+        if (!CheckIfCanProceed())
+        {
             return;
+        }
 
         zoom.NextTarget();
     }
 
-    // If the tutorial for the controls is still playing, we dont want the player to skip, pause or return. 
-    private bool checkIfCanProceed()
+    // If the tutorial for the controls is still playing, we don't want the player to skip, pause or return 
+    private bool CheckIfCanProceed()
     {
-        if (!controls.IsFinished)
-            return false;
-
-        return true; 
+        return controls.IsFinished;
     }
 }
+
+// Rinske's code
